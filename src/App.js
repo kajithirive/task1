583ec8e744d84b4801ac890d0138b0291c299054
Thiriveedhi <kaji.thiriveedhi@capgemini.com>
2025-07-01 22:06:06 +0530

    usestate, useeffect, props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Child from './Message.js';
 
-function App() {
+export default function App() {
+  const [index, setIndex] = useState("");
+  const [count, setcount] = useState(0);
+  async function kaji() {
+    const res = await  fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    console.log(data);
+    setIndex(data.slip.advice);
+    setcount(()=>count+1);
+    
+  }
+  useEffect (function(){kaji()},[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><h1>{index}</h1>
+    <button onClick={kaji}>Kaji</button>
+    <Child cont = {count}></Child></>
   );
 }
 
-export default App;
+
---
+++ src/Message.js
@@ -0,0 +1,6 @@
+import React from "react";
+export default function Message ({cont}) {
+    return(
+      <p>you have read {cont}</p>
+    );
+  }
